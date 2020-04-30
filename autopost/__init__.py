efa32cd08b9e4ae908e114623af17f0d307dafb6
Tasver <bohdannavrotskyi@gmail.com>
2020-04-30 18:15:53 +0300

    test  facebook3

--- autopost/__init__.py
+++ autopost/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 import uuid
-import json, facebook
+#import json, facebook
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
