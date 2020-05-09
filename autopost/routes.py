d0d8c6f9ab08304398c9bd5f2191cfcb55ac5e87
Tasver <bohdannavrotskyi@gmail.com>
2020-05-09 15:00:30 +0300

    rrrrrrrrrrr

--- autopost/routes.py
+++ autopost/routes.py
@@ -104,7 +104,7 @@ def add_task():
         #print(minute)
 
         #job2 = add.queue(3, 4, queue='high', timeout=60 * 2)
-        ob2 = queue.enqueue(facebook_create_post,facebook_login,facebook_password,test_publish,test_test)
+        ob2 = queue.enqueue(facebook_create_post,facebook_login,facebook_password,test_publish,test)
         #facebook_create_post(facebook_login, facebook_password, test_publish, url_image=test)
           # Outputs False as job is not enqueued
 
