3bd730f79c73b32a67a9df9cac6ac3fb1cf26dc4
Tasver <bohdannavrotskyi@gmail.com>
2020-05-09 14:05:09 +0300

    tes5

--- autopost/routes.py
+++ autopost/routes.py
@@ -79,6 +79,7 @@ def add_task():
             key = post.image_file
             #test = download(key)
             #test2 = download('test/600fffdef71457efeecc.png')
+            testt5 = 's3://dyploma-autopost2/test/26e1b96cbf41076b19ac.png'
             #test3=download('s3://dyploma-autopost2/test/098bd2beadf444c5519e.png')
             test = key
             #test = queue.enqueue(download, key)
@@ -101,7 +102,7 @@ def add_task():
         #print(minute)
 
         #job2 = add.queue(3, 4, queue='high', timeout=60 * 2)
-        ob2 = queue.enqueue(facebook_create_post,facebook_login,facebook_password,test_publish,test)
+        ob2 = queue.enqueue(facebook_create_post,facebook_login,facebook_password,test_publish,testt5)
         #facebook_create_post(facebook_login, facebook_password, test_publish, url_image=test)
           # Outputs False as job is not enqueued
 
