60ba72f8052ab5ff66f13be9aa417bf327617e8d
Tasver <bohdannavrotskyi@gmail.com>
2020-05-09 12:09:57 +0300

    new try4

--- autopost/routes.py
+++ autopost/routes.py
@@ -77,9 +77,9 @@ def add_task():
         test = None
         if post.image_file!=None and post.image_file!="no file":
             key = post.image_file
-            #test = download(key)
+            test = download(key)
             #test = key
-            test = queue.enqueue(download, key)
+            #test = queue.enqueue(download, key)
             print(test)
         else:
             test = None
@@ -95,9 +95,9 @@ def add_task():
         #print(day)
         #print(hour)
         #print(minute)
-        time.sleep(5)
+
         #job2 = add.queue(3, 4, queue='high', timeout=60 * 2)
-        ob2 = queue.enqueue(facebook_create_post,facebook_login,facebook_password,test_publish,url_image=test)
+        ob2 = queue.enqueue(facebook_create_post,facebook_login,facebook_password,test_publish,test)
         #facebook_create_post(facebook_login, facebook_password, test_publish, url_image=test)
           # Outputs False as job is not enqueued
 
