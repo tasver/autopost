099eb103c90e47984a5a938fb48e9f9195ffc840
Tasver <bohdannavrotskyi@gmail.com>
2020-05-14 14:38:57 +0300

    test queue6

--- autopost/routes.py
+++ autopost/routes.py
@@ -426,7 +426,7 @@ def publish_task(post_id):
                 job = queue.enqueue(facebook_create_post,soc.login,soc.password,test_publish,test )
                 post.job_id = str(job)
                 print(job)
-                job2 = queue.enqueue_in(timedelta(seconds=3), get_res(job,post))
+                job2 = queue.enqueue(get_res,job,post)
                 result_job = job.result
                 print("result job")
                 print(result_job)
@@ -502,7 +502,8 @@ def add_to_queue_task(post_id):
                 registry = ScheduledJobRegistry(queue=queue)
                 print(job in registry)
                 print('Job id: %s' % job.id)
-                job2 = queue.enqueue_in(timedelta(seconds=3), get_res,job,post)
+
+                job2 = queue.enqueue(get_res,job,post)
                 result_job = job.result
                 print("result job")
                 print(result_job)
